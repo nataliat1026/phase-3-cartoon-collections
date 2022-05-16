b9ba420422c24ceb17535fa6376d0dcf59684542
Natalia Teixeira <nataliat1026@gmail.com>
2022-05-16 13:04:42 -0400

    tests passing

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarf_names
+  dwarf_names.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+end
+# this also works:
+# dwarf_names.each_with_index do |dwarf, index|
+#   puts "#{index + 1}. #{dwarf}"
+
+
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map {|call| call.capitalize + '!'}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def long_planeteer_calls calls
+  calls.any? {|call| call.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese array
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|item| cheese_types.include? (item) }
 end
+
