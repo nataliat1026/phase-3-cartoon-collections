ed28dfe6eb0ffd676900862b0622fbf3d60b9cda
Natalia Teixeira <nataliat1026@gmail.com>
2022-05-16 15:33:59 -0400

    commit tree 0df034d2c068af0ad7b6251a75338681b0015c1a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,24 @@
-def roll_call_dwarves dwarf_names
-  dwarf_names.each.with_index(1) do |dwarf, index|
-    puts "#{index}. #{dwarf}"
+def roll_call_dwarves(dwarf_arr)
+  dwarf_arr.each.with_index(1) do |dwarf_name, index|
+    puts "#{index}. #{dwarf_name}"
   end
 end
 # this also works:
-# dwarf_names.each_with_index do |dwarf, index|
-#   puts "#{index + 1}. #{dwarf}"
+# dwarf_arr.each_with_index do |dwarf_name, index|
+#   puts "#{index + 1}. #{dwarf_name}"
 
 
-def summon_captain_planet planeteer_calls
-  planeteer_calls.map {|call| call.capitalize + '!'}
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call_name| call_name.capitalize + '!'}
 end
 
 
-def long_planeteer_calls calls
-  calls.any? {|call| call.length > 4}
+def long_planeteer_calls(calls)
+  calls.any? {|call_name| call_name.length > 4}
 end
 
 
-def find_the_cheese array
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|item| cheese_types.include? (item) }
 end
